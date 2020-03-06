37ca97e7ac411a3894d07d26f5787f997b532893
Clark <55460799+clarkyangjw@users.noreply.github.com>
2020-03-06 17:09:34 -0500

    Update contacts.h

--- contacts.h
+++ contacts.h
@@ -1,22 +1,6 @@
-// ----------------------------------------------------------------------------
-// DO NOT remove the below 2 lines.
-// The below two lines is needed to ensure only one copy
-// of this header file is included when compiling the project.
-// (it is referred to as "safeguarding")
-// You will learn about this in C++ (OOP244/BTP200)
-// For now, simply leave this code in!
 #ifndef CONTACTS_H_
 #define CONTACTS_H_
-// ----------------------------------------------------------------------------
 
-/* -------------------------------------------
-Name:Jiawei Yang
-Student number:121134183
-Date:Nov.17 2018
-----------------------------------------------
-Assignment: 2
-Milestone:  4
----------------------------------------------- */
 #define SIZEOFNUMBERS 11
 
 //------------------------------------------------------
